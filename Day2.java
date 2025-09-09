0f21cc2c9b6c9b314753a2b1b54a8a71f2cc269b
nazwazalbila <nazwazalbila4@gmail.com>
2025-09-09 14:55:10 +0700

    Create Day2.java

---
+++ Day2.java
@@ -0,0 +1,11 @@
+package com.mycompany.day2java;
+public class Day2java {
+
+    public static void main(String[] args) {
+        System.out.print("nazwa zalbila");
+        System.out.println("teknik informatika");
+        
+        int angkatan = 2025 ;
+        System.out.printf ("angkatan: %d", angkatan);
+    }
+}
