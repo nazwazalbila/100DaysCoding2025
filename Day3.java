a9f9d8351e7c1070a35d76f62fa19ba3f3092bde
nazwazalbila <nazwazalbila4@gmail.com>
2025-09-10 16:59:23 +0700

    Create Day3.java

---
+++ Day3.java
@@ -0,0 +1,13 @@
+package com.mycompany.day3.java;
+
+
+public class Day3Java {
+
+    public static void main(String[] args) {
+        System.out.println("komentar satu baris");
+        // ini komentar satu baris untuk penjelasan singkat
+        System.out.println("komentar multi baris ");
+        /*komentar ini komentar multi baris untuk penjelasan panjang */
+        
+    }
+}
