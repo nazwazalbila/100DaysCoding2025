9246dc321be850fcc571c36182517e1c24bd7837
nazwazalbila <nazwazalbila4@gmail.com>
2025-09-11 16:07:35 +0700

    Create Day4.java

---
+++ Day4.java
@@ -0,0 +1,24 @@
+package com.mycompany.day4.java;
+
+
+public class Day4Java {
+
+    public static void main(String[] args) {
+        
+        //penggunaan escape sequence di java
+        //(\n) newline untuk pindah ke baris berikutnya.
+        System.out.println("Nama\nNazwa zalbila");
+        //(\t) tab untuk memberi jarak seperti saat menekan tombol TAB
+        System.out.println("Fakultas:\tTeknik");
+        System.out.println("Prodi:\tInformatika");
+        //(\')tanda kutip tunggal untuk menampilkan tanda ' didalam string
+        System.out.println("class of\'b angkatan 2025");
+        //(\") tanda kutip ganda untuk menampilkan tanda " didalam string
+        System.out.println("motto hidup: \"hidup adalah ladang perang dan aku tidak datang untuk kalah\"");
+        //(\\) backlash untuk menampilkan karakter \ biasa
+        System.out.println("Alamat:\\desa palatta\\kec.tapango");
+        //(\b) backspace untuk menghapus karakter sebelumnya kalau kelihatan di console
+        System.out.println("suka menulis ceritaa\b");
+        
+    }
+}
