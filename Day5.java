885505b108b491ac1f2c4fff54d6c8a14dbf91af
nazwazalbila <nazwazalbila4@gmail.com>
2025-09-12 19:11:24 +0700

    Create Day5.java

---
+++ Day5.java
@@ -0,0 +1,25 @@
+package com.mycompany.day5.java;
+
+
+public class Day5Java {
+
+    public static void main(String[] args) {
+                
+// Tipe Data Numerik Bilangan Bulat (byte, short, int, long)
+        //byte (rentang nilai -128 sampai 127 digunakan untuk angka kecil)
+        byte umur =18;
+        //short (rentang nilai -32.768 sampai 32.767 untuk angka agak besar)
+        short tinggibadan = 155;
+        //int (rentang nilai -2.147.483.648 sampai 2.147.483.647 untuk angka besar)
+        int jumlahpendudukdidesa = 2000000;  
+        //long (rentang nilai ±9.223.372.036.854.775.808 untuk angka yang sangat )
+        long populasikabupaten = 6500000000L;
+        
+        System.out.println("umur:"+umur);
+        System.out.println("tinggi badan:"+tinggibadan);
+        System.out.println("jumlah penduduk didesa:"+jumlahpendudukdidesa);
+        System.out.println("populasi kabupaten:"+populasikabupaten);
+        
+               
+    }
+}
