b3c8f4cfe5a965f697ca40fc1f5f3be185dece18
nazwazalbila <nazwazalbila4@gmail.com>
2025-09-13 11:57:40 +0700

    Create Day6.java

---
+++ Day6.java
@@ -0,0 +1,16 @@
+package com.mycompany.day6.java;
+
+
+public class Day6Java {
+
+    public static void main(String[] args) {
+       //  Tipe Data Pecahan (double, float).
+       // float harus pakai f diakhir angka desimalnya presisi 7 digit
+       float angkakecil = 5.13f;
+       //double angka desimal panjang dan tidak perlu pakai f
+       double angkabesar = 5.1312976534532876;
+               
+        System.out.println("float :" + angkakecil);
+        System.out.println("double:" + angkabesar);
+    }
+}
