241e8e3928a2b459a96de39014f3a9b6506f16bd
nazwazalbila <nazwazalbila4@gmail.com>
2025-09-14 19:10:12 +0700

    Create Day7.java

---
+++ Day7.java
@@ -0,0 +1,18 @@
+package Day7;
+
+
+public class java {
+    public static void main(String[] args) {
+    // Tipe Data Karakter (char) dan Boolean (boolean).
+    // char untuk menyimpan satu karakter saja ditulis pakai tanda petik tunggal ' '
+    char kelas = 'B';
+    //boolean untuk menyimpan 2 nilai logika saja (true dan false)
+    boolean benar = true;
+    boolean salah = false;
+    
+      System.out.println("kelas : " + kelas);
+      System.out.println("saya mahasiswa?" + benar);
+      System.out.println("saya tidak kuliah?" + salah);
+    }
+    
+}
