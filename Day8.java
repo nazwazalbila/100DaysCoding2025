76b16e74010939f3e5a2d192ee4476f1a1b5a634
nazwazalbila <nazwazalbila4@gmail.com>
2025-09-15 16:24:32 +0700

    Create Day8.java

---
+++ Day8.java
@@ -0,0 +1,23 @@
+package com.mycompany.day8.java;
+
+public class Day8Java {
+
+    public static void main(String[] args) {
+      
+//String untuk menyimpan teks atau sekumpulan karakter
+//ditulis dengan tanda petik ganda "
+
+String unsulbar  = "Perguruan Tinggi Negeri di Majene";
+String informatika  = "Salah satu jurusan dengan banyak peminat di unsulbar";
+String tentangIT   = "Didalam Informatika kita belajar coding ";
+String coding  = "Menulis petunjuk supaya komputer mengerti dan melakukan sesuatu yang kita mau";
+
+    System.out.println("unsulbar: " + unsulbar);
+    System.out.println("informatika: " + informatika);
+    System.out.println("tentangIT: " + tentangIT);
+    System.out.println("coding: " + coding);
+
+        
+    }
+    
+}
