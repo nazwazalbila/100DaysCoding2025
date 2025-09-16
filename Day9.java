15805ff616a67186a3a2728c4f907b74f228a8a3
nazwazalbila <nazwazalbila4@gmail.com>
2025-09-16 19:05:27 +0700

    Create Day9.java

---
+++ Day9.java
@@ -0,0 +1,16 @@
+package com.mycompany.day9.java;
+
+
+public class Day9Java {
+
+    public static void main(String[] args) {
+        //deklarasi
+        int a;
+        //inisialisasi
+        a = 10;
+        System.out.println("nilai sebelum update : "+a);
+        //update variabel
+        a = 12;
+        System.out.println("nilai sesudah update : "+a);
+    }
+}
