9e660bbb6916eda186f18b60c980367fc31be869
nazwazalbila <nazwazalbila4@gmail.com>
2025-09-17 18:00:31 +0700

    Create Day10.java

---
+++ Day10.java
@@ -0,0 +1,20 @@
+package Day10;
+
+public class javaa {
+    public static void main(String[] args) {
+     //constanta (final variabel)
+     //variabel yg nilainya tidak bisa diiubah lagi setelah diinisialisasi jika diubah maka akan error
+     //gunakan final variabel untuk membuat nilai constanta
+     
+     final int bulandalamsetahun = 12;
+     final char golongandarah = 'A';
+     
+     
+        System.out.println("bulan dalam setahun : " + bulandalamsetahun);
+        System.out.println("golongan darah : " + golongandarah);
+        
+         
+     
+    }
+    
+}
