583c506006f8ae0538af7a50b75e16c7fb20f35d
nazwazalbila <nazwazalbila4@gmail.com>
2025-09-20 15:34:42 +0700

    Create Day13.java

---
+++ Day13.java
@@ -0,0 +1,11 @@
+public class Part3 {
+    public static void main(String[] args) {
+        System.out.println("\nBIODATA MAHASISWA");  
+        System.out.println("-------------------");
+        System.out.println("\t Nama  : Andi");   
+        System.out.println("\t Kelas : B");   
+        System.out.println("\t Status: \"Aktif\"");   
+           
+    }
+   
+}
