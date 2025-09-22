70a25b6f167ea58bd560bc8b970e1f10d54e4cb1
nazwazalbila <nazwazalbila4@gmail.com>
2025-09-22 18:01:01 +0700

    Create Day15.java

---
+++ Day15.java
@@ -0,0 +1,16 @@
+package day15.java;
+public class Day15Java {
+ public static void main(String[] args) {
+     
+ int jumlah = 50;
+ int harga = 45;
+ int uang = jumlah + harga;
+ int barang = jumlah - harga;
+ int hasil = jumlah + jumlah - harga;
+ 
+     System.out.println(uang);
+     System.out.println(barang);
+     System.out.println(hasil);
+ }
+    
+}
