17f94c27cc39e02e8cb3d6a101f980ec9e4fd81b
nazwazalbila <nazwazalbila4@gmail.com>
2025-09-23 16:33:53 +0700

    Create Day16.java

---
+++ Day16.java
@@ -0,0 +1,16 @@
+package day16.java;
+public class Day16Java {
+ public static void main(String[] args) {
+     
+     byte angka = 60; 
+     byte bulan = 12;
+     int dana = angka * bulan;
+     int depan = angka / bulan;
+     int danabulandepan = angka * angka / bulan;
+     
+     System.out.println(dana);
+     System.out.println(depan);
+     System.out.println(danabulandepan); 
+    }
+    
+}
