0150786e939d744cfa681969b0ef7e5cb5cdb5f7
nazwazalbila <nazwazalbila4@gmail.com>
2025-09-24 20:34:08 +0700

    Update Day17.java

--- Day17.java
+++ Day17.java
@@ -6,10 +6,10 @@ public class Day17Java {
      
       int a = 25;
       int b = 4;
-      int c = (25 % 4);
+      int c = (a % b);
       int d = 30;
       int e = 15;
-      int f = (30 % 15);
+      int f = (d % e);
         System.out.println(c);
         System.out.println(f);
         
