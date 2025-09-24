102d0a48b185e4cfe8d272845cf9cb8448d0f824
nazwazalbila <nazwazalbila4@gmail.com>
2025-09-24 13:45:07 +0700

    Create Day17.java

---
+++ Day17.java
@@ -0,0 +1,18 @@
+package day17.java;
+
+public class Day17Java {
+    public static void main(String[] args) {
+        
+     
+      int a = 25;
+      int b = 4;
+      int c = (25 % 4);
+      int d = 30;
+      int e = 15;
+      int f = (30 % 15);
+        System.out.println(c);
+        System.out.println(f);
+        
+    }
+    
+}
