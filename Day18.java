067e8d27dad3a9666c81aaeda4a08a3b31f8b437
nazwazalbila <nazwazalbila4@gmail.com>
2025-09-25 17:49:29 +0700

    Create Day18.java

---
+++ Day18.java
@@ -0,0 +1,13 @@
+package Day18;
+
+public class java {
+    public static void main(String[] args) {
+        
+        byte angka = 120;
+        int angkalebihbesar = angka;
+        
+        System.out.println("byte : " + angka);
+        System.out.println("int : " + angkalebihbesar);
+    }
+  
+}
