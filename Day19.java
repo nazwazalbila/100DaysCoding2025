0f460434f9d359fa16ef7520c012fce7102a212a
nazwazalbila <nazwazalbila4@gmail.com>
2025-09-26 17:29:09 +0700

    Create Day19.java

---
+++ Day19.java
@@ -0,0 +1,11 @@
+package day19;
+public class java {
+    public static void main(String[] args) {
+        
+        double tb = 155.45;
+        int tbsaya = (int) tb;
+        
+        System.out.println("Tinggi asli : " +tb);
+        System.out.println("Tinggi  : " +tbsaya);
+    }
+}
