ec0bf702db5ddc35bd4b7e7ca6e07ecbaa5f1e5f
nazwazalbila <nazwazalbila4@gmail.com>
2025-09-27 20:09:53 +0700

    Update Day20.java

--- Day20.java
+++ Day20.java
@@ -3,11 +3,20 @@ public class Day20Java {
     
     public static void main(String[] args) {
         
+        // menggunakan string.valueOf
         int jumlah = 120;
         String angkajumlah = String.valueOf(jumlah);
-        
         System.out.println("Nilai jumlah dalam bentuk string : " +angkajumlah);
         
+        //menggunakan +"" (contact string)
+        int harga = 50;
+        String hargabarang= harga + "";
+        System.out.println("Nilai harga dalam bentuk string : " +hargabarang);
+        
+        // menggunakan Integer.toString() (atau Double.toString(), dll)
+        double berat = 35;
+        String beratbenda = Double.toString(berat);
+        System.out.println("Nilai berat dalam bentuk string : " +beratbenda);
             }
     
 }
