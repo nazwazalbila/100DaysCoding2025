1408a84988178882678ebc959b66e27293c21cf2
nazwazalbila <nazwazalbila4@gmail.com>
2025-09-27 18:13:07 +0700

    Create Day20.java

---
+++ Day20.java
@@ -0,0 +1,13 @@
+package day20.java;
+public class Day20Java {
+    
+    public static void main(String[] args) {
+        
+        int jumlah = 120;
+        String angkajumlah = String.valueOf(jumlah);
+        
+        System.out.println("Nilai jumlah dalam bentuk string : " +angkajumlah);
+        
+            }
+    
+}
