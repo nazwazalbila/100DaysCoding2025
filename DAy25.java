e32fd52e49fc676f2573fc0235b4abb73cfb1cb4
nazwazalbila <nazwazalbila4@gmail.com>
2025-10-02 18:55:32 +0700

    Create DAy25.java

---
+++ DAy25.java
@@ -0,0 +1,21 @@
+package day25.javaa;
+
+public class Day25Javaa {
+
+        public static void main(String[] args) {
+        
+            //menggunakan bentuk prefix
+            int a =8;
+            int b = ++a;
+            System.out.println(a);
+            System.out.println(b);
+            
+            //menggunakan bentuk postfix
+            int c = 10;
+            int d = c--;
+            System.out.println(c);
+            System.out.println(d);
+            
+    }
+    
+}
