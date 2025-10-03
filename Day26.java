5a47e96856d830b94b6589f334584f843473a5fc
nazwazalbila <nazwazalbila4@gmail.com>
2025-10-03 10:09:28 +0700

    Create Day26.java

---
+++ Day26.java
@@ -0,0 +1,32 @@
+package day26.java;
+
+public class Day26Java {
+
+    public static void main(String[] args) {
+        
+        int angka = 3;
+         System.out.println("Nilai awal angka : " +angka);
+         
+        angka +=10;
+         System.out.println("Nilai angka setelah += : " +angka);
+         
+        angka -=2;
+         System.out.println("Nilai angka setelah -= : " +angka);
+
+        angka *=9;
+        System.out.println("Nilai angka setelah *=: " +angka);
+        
+        angka /=3;
+         System.out.println("Nilai angka setelah /= : " +angka);
+         
+        angka %=8;
+         System.out.println("Nilai angka setelah %= : " +angka);
+       
+       
+               
+       
+       
+    
+    }
+    
+}
