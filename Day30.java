771daf3beed50f97614482d9f411d31a825cf65f
nazwazalbila <nazwazalbila4@gmail.com>
2025-10-07 16:32:13 +0700

    Create Day30.java

---
+++ Day30.java
@@ -0,0 +1,17 @@
+package day30.javaa;
+
+
+public class Day30Javaa {
+
+    public static void main(String[] args) {
+        
+        int a = 5;
+        int b = 8;
+        
+        if (a<=b){
+            System.out.println("a lebih kecil atau sama dengan b");
+        }  else if (a>=b){
+             System.out.println("a lebih besar atau sama dengan b");       
+                    }    }
+    
+}
