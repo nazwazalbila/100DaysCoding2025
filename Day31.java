b0ad31a9ea561b5048c7c1fe1cc9edb3fbb9b6ed
nazwazalbila <nazwazalbila4@gmail.com>
2025-10-08 14:40:07 +0700

    Create Day31.java

---
+++ Day31.java
@@ -0,0 +1,19 @@
+
+package day31.java;
+
+
+public class Day31Java {
+
+    
+    public static void main(String[] args) {
+       int umur = 18;
+       boolean ktp =true;
+       
+       if (umur >= 17 && ktp ){
+           System.out.println("anda sudah bisa membuat ktp");
+       }else{
+           System.out.println("anda belum bisa membuat ktp");
+       }
+    }
+    
+}
