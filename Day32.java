13f7d29e2ae072b78531d2a1930adf8d473c6c4d
nazwazalbila <nazwazalbila4@gmail.com>
2025-10-09 14:30:56 +0700

    Create Day32.java

---
+++ Day32.java
@@ -0,0 +1,17 @@
+package day32.java;
+
+public class Day32Java {
+
+    public static void main(String[] args) {
+        
+        boolean senin = false;
+        boolean minggu = true;
+        
+        if (senin || minggu){
+            System.out.println("tetap kerja , ini bukan hari libur");
+        }else {
+            System.out.println("ini hari libur, kamu bisa bersantai");
+        }
+    }
+    
+}
