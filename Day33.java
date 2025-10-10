6d0214dad0912445f26218c1698b556de5a186f3
nazwazalbila <nazwazalbila4@gmail.com>
2025-10-10 18:44:58 +0700

    Create Day33.java

---
+++ Day33.java
@@ -0,0 +1,16 @@
+package day33.java;
+
+public class Day33Java {
+
+    public static void main(String[] args) {
+        
+        boolean fakta =true;
+        
+        if (!fakta){
+            System.out.println("saya suka makan seblak");
+        }else {
+            System.out.println("saya tidak suka makan seblak");
+        }
+    }
+    
+}
