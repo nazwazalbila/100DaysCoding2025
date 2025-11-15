b984647e3dff5bc61d9d0ad6ad8aaddd302127ea
nazwazalbila <nazwazalbila4@gmail.com>
2025-11-15 20:11:19 +0700

    Create Day69.java

---
+++ Day69.java
@@ -0,0 +1,16 @@
+package evaluasi.mentorsoal1;
+
+import java.util.Scanner;
+
+public class soal2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        
+     System.out.print(" jumlah jam :");
+        int jam = input.nextInt();
+        int menit = jam * 60;
+        System.out.println(menit);
+        
+    }
+   
+}
