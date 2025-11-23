768f003f613008a62fd302fa6e249057e38a9519
nazwazalbila <nazwazalbila4@gmail.com>
2025-11-23 21:27:59 +0700

    Refactor Day77.java to use hardcoded string

--- Day77.java
+++ Day77.java
@@ -1,15 +1,10 @@
 package Day77;
-
-import java.util.Scanner;
-
 public class java {
     public static void main(String[] args) {
-        Scanner in = new Scanner (System.in);
         
-        System.out.print("Masukkan kalimat :");
-        String kalimat = in.nextLine();
+        String kalimat = " Pasti lulus matkul ddp ";
         
-        System.out.println("Substring (0-4): " + kalimat.substring(0, 4));
+        System.out.println("Substring (0-6): " + kalimat.substring(0, 6));
         System.out.println("Ganti 'a' jadi 'e': " + kalimat.replace("a", "e"));
         System.out.println("Hapus spasi: '" + kalimat.trim() + "'");
     }
