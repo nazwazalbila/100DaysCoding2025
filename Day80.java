d5c53ca7f3e7daf96c03516bdbc38c6231a65134
nazwazalbila <nazwazalbila4@gmail.com>
2025-11-26 18:33:02 +0700

    Create Day80.java

---
+++ Day80.java
@@ -0,0 +1,13 @@
+package Day80;
+public class java {
+    public static void main(String[] args) {
+        
+        int[] nilai = {5, 10, 15, 20, 25};
+
+        System.out.println("Isi Array:");
+        for (int i = 0; i < nilai.length; i++) {
+            System.out.println("Index ke " + i + " = " + nilai[i]);
+        }
+    }
+ 
+}
