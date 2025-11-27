a4c25709355150927177377086eb801f41d609ba
nazwazalbila <nazwazalbila4@gmail.com>
2025-11-27 18:55:27 +0700

    Add Day81.java with animal index printing

---
+++ Day81.java
@@ -0,0 +1,12 @@
+package Day81;
+
+public class java {
+    public static void main(String[] args) {
+        
+        String[] hewan = {"Kucing", "Anjing", "Kelinci", "Beruang", "Ikan"};
+
+        System.out.println("Indeks ke 0 = " + hewan[0]);
+        System.out.println("Indeks ke 2 = " + hewan[2]);
+        System.out.println("Indeks ke 4 = " + hewan[4]);
+    }  
+}
