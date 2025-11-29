ad709b74498ce164d40c095b02c265cc9658a9a9
nazwazalbila <nazwazalbila4@gmail.com>
2025-11-29 19:55:25 +0700

    Add Day83.java to calculate array total

---
+++ Day83.java
@@ -0,0 +1,14 @@
+package Day83;
+public class java {
+    public static void main(String[] args) {
+        int[] angka = {4, 8, 12, 16, 20};
+        int total = 0;
+
+        for (int i = 0; i < angka.length; i++) {
+            total += angka[i];
+        }
+        System.out.println("Total jumlah elemen array = " + total);
+    
+    }
+   
+}
